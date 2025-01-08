4ac223389e6bd56f8376694d7b511bf20aeedcf8
tsy-dev <tohasyafingi12@gmail.com>
2025-01-08 20:27:59 +0700

    AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') != 'local') {
-            \URL::forceScheme('https');
-        }
+        //
     }
     protected $policies = [
         Blog::class => BlogPolicy::class,
